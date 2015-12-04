a14a6794cef286a724b324a99de2af383e2c9a58
zhityer <barcelona20202@gmail.com>
2015-12-04 17:40:32 +0330

    Update groupmanager.lua

--- plugins/groupmanager.lua
+++ plugins/groupmanager.lua
@@ -299,19 +299,19 @@ function run(msg, matches)
             end
 		end
 		    if matches[1] == 'link' then
-      local chat = 'chat#id'..msg.to.id
-      if matches[2] == 'get' then
-        if data[tostring(msg.to.id)]['link'] then
+          local chat = 'chat#id'..msg.to.id
+          if matches[2] == 'get' then
+          if data[tostring(msg.to.id)]['link'] then
           local about = get_description(msg, data)
           local link = data[tostring(msg.to.id)]['link']
           return about.."\n\n"..link
-        else
-          return "Invite link is not exist.\nTry !link set to generate it."
+          else
+           return "Invite link is not exist.\nTry !link set to generate it."
+          end
+         end
+          if matches[2] == 'set' and is_sudo(msg) then
+         msgr = export_chat_link('chat#id'..msg.to.id, export_chat_link_callback, {receiver=receiver, data=data, chat_id=msg.to.id, group_name=msg.to.print_name})
         end
-      end
-      if matches[2] == 'set' and is_sudo(msg) then
-        msgr = export_chat_link('chat#id'..msg.to.id, export_chat_link_callback, {receiver=receiver, data=data, chat_id=msg.to.id, group_name=msg.to.print_name})
-      end
 	  end
 		if matches[1] == 'chat_change_photo' and msg.from.id ~= 0 then
 		    if not msg.service then
